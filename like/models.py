9cec496e9e6a12d9db26b8dbf67dd0eaba81fad8
smile <smile@dj.com>
2020-01-02 21:31:56 +0800

    添加了like模型

--- like/models.py
+++ like/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class like(models.Model):
+    uid = models.IntegerField()
+    wid = models.IntegerField()
+
+    created = models.DataTimeField()
